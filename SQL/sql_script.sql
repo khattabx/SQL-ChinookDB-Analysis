901ce3998dd00aeb97a170f9181cd0455bbe4bd2
Ahmed Yasser <ahvadd.k@gmail.com>
2025-08-15 21:17:48 +0300

    Add Sql Queries
    - answer the business questions

--- SQL/sql_script.sql
+++ SQL/sql_script.sql
@@ -5,3 +5,63 @@
 	-- Monthly Performance?
 
 USE Chinook
+
+-- Q1) Top-Selling Products? -------------------------------------------------
+
+SELECT TOP 10
+    T.Name AS Product_Name,
+    SUM(IL.Quantity) AS Total_Quantity_Sold,
+    SUM(IL.Quantity * IL.UnitPrice) AS Total_Revenue
+FROM
+    InvoiceLine AS IL
+JOIN
+    Track AS T ON IL.TrackId = T.TrackId
+GROUP BY
+    T.Name
+ORDER BY
+    Total_Quantity_Sold DESC;
+
+-- Q2) Revenue Per Region? ---------------------------------------------------
+
+SELECT
+    I.BillingCountry AS Region,
+    SUM(I.Total) AS Total_Revenue
+FROM
+    Invoice AS I
+GROUP BY
+    I.BillingCountry
+ORDER BY
+    Total_Revenue DESC;
+
+-- Q3) Monthly Performance? ---------------------------------------------------
+
+SELECT
+    FORMAT(InvoiceDate, 'yyyy-MM') AS Sales_Month,
+    SUM(Total) AS Monthly_Revenue
+FROM
+    Invoice
+GROUP BY
+    FORMAT(InvoiceDate, 'yyyy-MM')
+ORDER BY
+    Sales_Month;
+
+-- Window Function : Top 3 Tracks by UnitPrice within each Genre using ROW_NUMBER 
+WITH RankedTracks AS (
+    SELECT
+        T.Name AS TrackName,
+        G.Name AS GenreName,
+        T.UnitPrice,
+        ROW_NUMBER() OVER (PARTITION BY G.Name ORDER BY T.UnitPrice DESC) AS RankByUnitPrice
+    FROM
+        Track AS T
+    JOIN
+        Genre AS G ON T.GenreId = G.GenreId
+)
+SELECT
+    TrackName,
+    GenreName,
+    UnitPrice
+FROM
+    RankedTracks
+WHERE
+    RankByUnitPrice <= 3;
