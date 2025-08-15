9978f79e3731ac202c0dd986e3e644fc370564aa
Ahmed Yasser <ahvadd.k@gmail.com>
2025-08-15 19:10:41 +0300

    Add Project Content
    - add data base sql_script [MySql, SqlServer]
    - add sql process .md file
    - add sql script .spl file

---
+++ SQL/sql_script.sql
@@ -0,0 +1,7 @@
+-- The script is designed to answer the business questions for the Elevvo training.
+---Business Questions:
+	-- Top-Selling Products?
+	-- Revenue Per Region?
+	-- Monthly Performance?
+
+USE Chinook
